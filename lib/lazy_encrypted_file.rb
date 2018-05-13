d61132129ff4a286cae2f5bcf8a75803a8125654
donkey_hotei <donkey-hotei@cryptolab.net>
2018-05-12 21:33:56 -0700

    add LazyEncryptedFile implementation

--- lib/lazy_encrypted_file.rb
+++ lib/lazy_encrypted_file.rb
@@ -1,4 +1,25 @@
 
 class LazyEncryptedFile
-  def initialize(file, cipher, taglen=16); end
+  attr_reader :file, :cipher, :taglen
+
+  def initialize(file, cipher, taglen=16)
+    @file = file
+    @cipher = cipher
+    @taglen = taglen
+    @size = file.size + taglen
+
+    file.seek(0)
+  end
+
+  def read(n_bytes)
+    chunk = file.read(n_bytes)
+    chunk = encrypt_chunk(chunk) if chunk
+    return chunk
+  end
+
+  private
+
+  def encrypt_chunk(chunk)
+    cipher.update(chunk) + cipher.final + cipher.auth_tag(taglen)
+  end
 end
--- test/test_lazy_encrypted_file.rb
+++ test/test_lazy_encrypted_file.rb
@@ -1,7 +1,47 @@
-require "minittest/autorun"
+require "hkdf"
+require "openssl"
+require "minitest/autorun"
 require "lazy_encrypted_file"
 
 class LazyEncryptedFileTest < Minitest::Test
-  def test_read_reads_successive_chunks_of_file
+  attr_reader :test_file
+
+  def setup
+    test_file = File.open("test.bin", "wb")
+    data = (0...50).map { ("a".."z").to_a[rand(26)] }.join
+    test_file.write(data)
+    test_file.close
+    @test_file = File.open("test.bin", "rb")
+  end
+
+  def test_read_encrypts_successive_chunks_of_file
+    plain_chunk = test_file.read(8)
+    lazy_encrypted_file = LazyEncryptedFile.new(test_file, cipher)
+    encrypted_chunk = lazy_encrypted_file.read(8)
+
+    cipher.reset
+    cipher.decrypt
+
+    decrypted_chunk = cipher.update(encrypted_chunk)[0...8]  # remove tag
+
+    assert plain_chunk == decrypted_chunk
+  end
+
+  private
+
+  def cipher
+    cipher = OpenSSL::Cipher.new("aes-128-gcm")
+    cipher.key = key
+    cipher.iv  = iv
+    cipher.encrypt
+    cipher
+  end
+
+  def key
+    @key ||= Random.new.bytes(16)
+  end
+
+  def iv
+    @iv ||= Random.new.bytes(12)
   end
 end
